837bc73f3e7ba88fd9777f6609a1fe821e1d394b
felipe ls <feliph_ls@hotmail.com>
2021-04-12 21:17:53 -0500

    Projects in mind section, and fixing the menu errors

--- js/app.js
+++ js/app.js
@@ -4,12 +4,12 @@ const toggleMenu = document.getElementById('nav-toggle');
 const closeMenu = document.getElementById('nav-close');
 
 /* Show Menu */ 
-toggleMenu.addEventListener('click', (ev) => {
+toggleMenu.addEventListener('click', () => {
     navMenu.classList.toggle('show');
 });
 
 /* Hide Menu */
-closeMenu.addEventListener('click', (ev) => {
+closeMenu.addEventListener('click', () => {
     navMenu.classList.remove('show');
 });
 
@@ -21,34 +21,94 @@ const linkAction = () => navMenu.classList.remove('show');
 navLink.forEach(l => l.addEventListener('click', linkAction));
 
 /* Scroll, action, Active line */
+const home = document.querySelector('#home__link');
+const about = document.querySelector('#about__link');
+const skills = document.querySelector('#skills__link');
+const education = document.querySelector('#education__link');
+const services = document.querySelector('#services__link');
+const works = document.querySelector('#works__link');
+const contact = document.querySelector('#contact__link');
 
-const sections = document.querySelectorAll('section[id]');
+home.addEventListener('click', () => {
+    home.classList.add('active');
+    about.classList.remove('active');
+    skills.classList.remove('active');
+    education.classList.remove('active');
+    services.classList.remove('active');
+    works.classList.remove('active');
+    contact.classList.remove('active');
 
-const scrollActive = (ev) => { 
-    const scrollY = window.pageYOffset;
-    sections.forEach( current => {
-        console.log(current);
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        const sectionId = current.getAttribute('id');
+});
 
-        if (scrollY> sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a [href*=' + sectionId + ']').classList.add('active')
-        } else {
-            document.querySelector('.nav__menu a [href*=' + sectionId + ']').classList.remove('active')
-        }
+about.addEventListener('click', () => {
+    home.classList.remove('active');
+    about.classList.add('active');
+    skills.classList.remove('active');
+    education.classList.remove('active');
+    services.classList.remove('active');
+    works.classList.remove('active');
+    contact.classList.remove('active');
 
-    });
-}
+});
 
-window.addEventListener('scroll', scrollActive);
+skills.addEventListener('click', () => {
+    home.classList.remove('active');
+    about.classList.remove('active');
+    skills.classList.add('active');
+    education.classList.remove('active');
+    services.classList.remove('active');
+    works.classList.remove('active');
+    contact.classList.remove('active');
 
+});
+
+education.addEventListener('click', () => {
+    home.classList.remove('active');
+    about.classList.remove('active');
+    skills.classList.remove('active');
+    education.classList.add('active');
+    services.classList.remove('active');
+    works.classList.remove('active');
+    contact.classList.remove('active');
+
+});
+
+services.addEventListener('click', () => {
+    home.classList.remove('active');
+    about.classList.remove('active');
+    skills.classList.remove('active');
+    education.classList.remove('active');
+    services.classList.add('active');
+    works.classList.remove('active');
+    contact.classList.remove('active');
+
+});
+
+works.addEventListener('click', () => {
+    home.classList.remove('active');
+    about.classList.remove('active');
+    skills.classList.remove('active');
+    education.classList.remove('active');
+    services.classList.remove('active');
+    works.classList.add('active');
+    contact.classList.remove('active');
+
+});
+
+contact.addEventListener('click', () => {
+    home.classList.remove('active');
+    about.classList.remove('active');
+    skills.classList.remove('active');
+    education.classList.remove('active');
+    services.classList.remove('active');
+    works.classList.remove('active');
+    contact.classList.add('active');
+
+});
 /* Cursor Dot*/
 const sigueRaton = document.getElementById('sigueRaton').style;
 
-const seguir = () => {
-  sigueRaton.top = parseInt(window.event.pageY + 5) + "px";
-  sigueRaton.left = parseInt(window.event.pageX + 5) + "px";
-}
-
-window.addEventListener('mouseover', seguir);
+window.addEventListener('mouseover', () => {
+    sigueRaton.top = parseInt(window.event.pageY + 5) + "px";
+    sigueRaton.left = parseInt(window.event.pageX + 5) + "px";
+});
